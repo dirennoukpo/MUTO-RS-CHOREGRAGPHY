01c2f71ac41f8f4f6ee72c157a86d4f9d6de262d
Edwin Diren Regis Mahugnon NOUKPO <diren.noukpo@epitech.eu>
2026-03-12 11:54:03 +0100

    feat: : Enhance CheckBatteryLevel node with atomic variables and battery threshold input

--- src/checkbatterylevel/include/checkbatterylevel/batterylevel.hpp
+++ src/checkbatterylevel/include/checkbatterylevel/batterylevel.hpp
@@ -5,13 +5,14 @@
 ** Login   <diren.noukpo@epitech.eu>
 **
 ** Started on  Thu Mar 12 8:11:48 AM 2026 dirennoukpo
-** Last update Fri Mar 12 10:28:39 AM 2026 dirennoukpo
+** Last update Fri Mar 12 11:52:13 AM 2026 dirennoukpo
 */
 
 #pragma once
+#include <atomic>
+
 #include "rclcpp/rclcpp.hpp"
 #include "behaviortree_cpp_v3/action_node.h"
-#include "behaviortree_cpp_v3/bt_factory.h"
 #include "std_msgs/msg/float32.hpp"
 
 class CheckBatteryLevel : public BT::SyncActionNode
@@ -20,8 +21,10 @@ class CheckBatteryLevel : public BT::SyncActionNode
         CheckBatteryLevel(const std::string &name, const BT::NodeConfiguration &config);
         static BT::PortsList providedPorts();
         BT::NodeStatus tick() override;
+
     private:
-        double _level{0};
+        std::atomic<float> _level{0.0F};
+        std::atomic<bool> _has_level{false};
         rclcpp::Node::SharedPtr _node;
         rclcpp::Subscription<std_msgs::msg::Float32>::SharedPtr _sub;
 };
--- src/checkbatterylevel/src/batterylevel.cpp
+++ src/checkbatterylevel/src/batterylevel.cpp
@@ -5,10 +5,11 @@
 ** Login   <diren.noukpo@epitech.eu>
 **
 ** Started on  Thu Mar 12 8:09:51 AM 2026 dirennoukpo
-** Last update Fri Mar 12 10:48:16 AM 2026 dirennoukpo
+** Last update Fri Mar 12 11:52:46 AM 2026 dirennoukpo
 */
 
-#include "../include/checkbatterylevel/batterylevel.hpp"
+#include "checkbatterylevel/batterylevel.hpp"
+#include "behaviortree_cpp_v3/bt_factory.h"
 
 CheckBatteryLevel::CheckBatteryLevel(const std::string &name, const BT::NodeConfiguration &config)
   : BT::SyncActionNode(name, config)
@@ -25,6 +26,7 @@ CheckBatteryLevel::CheckBatteryLevel(const std::string &name, const BT::NodeConf
         [this] (const std_msgs::msg::Float32::SharedPtr value)
         {
             _level = value->data;
+            _has_level = true;
         }
     );
 }
@@ -34,18 +36,31 @@ BT::PortsList CheckBatteryLevel::providedPorts()
     return
     {
         BT::InputPort<std::string>("robot_id"),
+        BT::InputPort<float>("min_battery_level", 0.0F, "Battery threshold"),
         BT::OutputPort<float>("battery_level")
     };
 }
 
 BT::NodeStatus CheckBatteryLevel::tick()
 {
-    // float threshold = 7.0;
-    
-    // if (_level < threshold) {
-    //     return BT::NodeStatus::FAILURE;
-    // }
-    setOutput("battery_level", _level);
+    rclcpp::spin_some(_node);
+
+    if (!_has_level.load()) {
+        return BT::NodeStatus::FAILURE;
+    }
+
+    float threshold = 0.0F;
+    if (auto min_battery_level = getInput<float>("min_battery_level")) {
+        threshold = min_battery_level.value();
+    }
+
+    const float level = _level.load();
+    setOutput("battery_level", level);
+
+    if (level < threshold) {
+        return BT::NodeStatus::FAILURE;
+    }
+
     return BT::NodeStatus::SUCCESS;
 }
 
