59578c910285a1f4b4bab4025e364cf66e34e632
Edwin Diren Regis Mahugnon NOUKPO <diren.noukpo@epitech.eu>
2026-03-13 15:29:48 +0100

    feat: Add header and source files for pkg_new_position package with initial documentation

---
+++ src/pkg_new_position/include/pkg_new_position/new_position.hpp
@@ -0,0 +1,12 @@
+/*
+** new_position.hpp for MUTO-RS-CHOREGRAGPHY [WSL: Ubuntu-22.04] in /home/edwin/MUTO-RS-CHOREGRAGPHY/src/pkg_new_position/include/pkg_new_position
+**
+** Made by dirennoukpo
+** Login   <diren.noukpo@epitech.eu>
+**
+** Started on  Fri Mar 13 3:28:05 PM 2026 dirennoukpo
+** Last update Sat Mar 13 3:28:06 PM 2026 dirennoukpo
+*/
+
+#pragma once
+
--- src/pkg_new_position/src/new_position.cpp
+++ src/pkg_new_position/src/new_position.cpp
@@ -1,10 +1,9 @@
-#include <cstdio>
-
-int main(int argc, char ** argv)
-{
-  (void) argc;
-  (void) argv;
-
-  printf("hello world pkg_new_position package\n");
-  return 0;
-}
+/*
+** new_position.cpp for MUTO-RS-CHOREGRAGPHY [WSL: Ubuntu-22.04] in /home/edwin/MUTO-RS-CHOREGRAGPHY/src/pkg_new_position/src
+**
+** Made by dirennoukpo
+** Login   <diren.noukpo@epitech.eu>
+**
+** Started on  Fri Mar 13 3:28:12 PM 2026 dirennoukpo
+** Last update Sat Mar 13 3:28:17 PM 2026 dirennoukpo
+*/
