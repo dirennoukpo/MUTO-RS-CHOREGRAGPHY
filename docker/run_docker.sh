b7e2fa28abc1e10be519c78e7a5327aee61dbb52
Edwin Diren Regis Mahugnon NOUKPO <diren.noukpo@epitech.eu>
2026-03-13 12:19:57 +0100

    feat: Enhance Docker setup with user and group arguments, update entrypoint permissions

--- docker/run_docker.sh
+++ docker/run_docker.sh
@@ -5,6 +5,8 @@ cd "$(dirname "$0")/.."
 
 export HOST_UID="$(id -u)"
 export HOST_GID="$(id -g)"
+export HOST_USER="$(id -un)"
+export HOST_GROUP="$(id -gn)"
 
 docker compose down --remove-orphans >/dev/null 2>&1 || true
 docker compose up -d --build --force-recreate
